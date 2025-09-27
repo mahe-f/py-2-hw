0946ebbd36b34e9f7bb4c53da3c9da1a8724ee14
mahe-f <fmaahekaan@gmail.com>
2025-09-27 22:25:59 +0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,14 @@
+mybd="05 september 2010"
+print("my birthday in on ",mybd)
+
+wbd="09 october 2010"
+print("wafia's birthday in on ",wbd)
+
+abd="06 june 2010"
+print("Amna's birthday in on ",abd)
+
+ashubd="06 december 2022"
+print("ashs's birthday in on ",ashubd)
+
+sbd="22 november 2011"
+print("salar's birthday in on ",sbd)
